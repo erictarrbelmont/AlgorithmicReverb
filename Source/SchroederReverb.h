98aa95b5d0c2d360b5baed3c2a6ccddf6a2694f3
arweidman <alek.weidman@gmail.com>
2024-04-18 14:49:00 -0500

    Update to code foundation
    Slight changes were made for foundational layers of the class. Comments were also added for clarity

--- Source/SchroederReverb.h
+++ Source/SchroederReverb.h
@@ -9,3 +9,50 @@
 */
 
 #pragma once
+#pragma once
+#include "FBCF.h"
+#include "APF.h"
+
+class SchroederReverb {
+    
+public:
+    
+    // Standard prepare function
+    void prepare(double sampleRate){
+        Fs = sampleRate;
+    }
+    
+    // Setting the gain associated with the FBCF blocks.
+    void setFBGain(float inputGain) {
+        FBGain = inputGain;
+    }
+    
+    // Setting the gain associated with the APF blocks
+    void setAPGain(float inputGain) {
+        APGain = inputGain;
+    }
+    
+    // Processing by sample
+    void processSample (float x, const int c) {}
+    
+    // Processing by buffer
+    void process (float * buffer, const int N, const int c) {}
+    
+    
+private:
+    
+    double Fs = 48000.f;
+    
+    float FBGain = 0.f; // Affects length of reverb, one value will be applied to all intances of FBCF
+    float APGain = 0.f; // Affects Diffusion, one value will be applied to all intances of APF
+    
+    // Intances for each FBCF and APF filters labeled accordingly
+    FBCF firstDelayBlock;
+    FBCF secondDelayBlock;
+    FBCF thirdDelayBlock;
+    FBCF fourthDelayBlock;
+    
+    APF firstAPFFilter;
+    APF secondAPFFilter;
+    
+};
