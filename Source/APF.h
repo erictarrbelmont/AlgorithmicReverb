d00b6c04697ba9ba11ea11d0797b2a87a27dd4b3
frankerino44 <fburgarino@gmail.com>
2024-04-20 21:29:29 -0500

    Fleshed out the APF.

--- Source/APF.h
+++ Source/APF.h
@@ -3,21 +3,49 @@
 
     APF.h
     Created: 18 Apr 2024 12:21:13pm
-    Author:  Eric Tarr
+    Author:  Franke Burgarino
 
   ==============================================================================
 */
 
 #pragma once
+#include "FractionalDelay.hpp"
 
 class APF {
 
 public: 
 
+    APF();
+    ~APF();
 
+    void setFs(float Fs);
+
+    void setDepth(float depth);
+    float getDepth();
+
+    void setSpeed(float speed);
+    float getSpeed();
+
+    void setDelay(float delaySamples);
+    float getDelay();
+
+    void setGain(float gain);
+    float getGain();
+
+    float processSample(float x, int channel);
 
 private:
 
+    float Fs;
+    float delaySamples; // delay time in samples
+    float gain;
 
+    float depth; // LFO depth for FractionalDelay
+    float speed; // LFO speed (Hz) for FractionalDelay
+    
+    FractionalDelay fracDelay;
+
+    float delayInput;
+    float delayOutput = 0.f;
 
 };
