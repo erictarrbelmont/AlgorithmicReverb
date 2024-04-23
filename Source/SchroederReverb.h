ac3b413fc36419c3230841f6d6d1c21d4f02814b
arweidman <alek.weidman@gmail.com>
2024-04-23 13:12:23 -0500

    Changes made to shroeder reberb CPP and H file
    Title ^

--- Source/SchroederReverb.h
+++ Source/SchroederReverb.h
@@ -33,18 +33,17 @@ public:
     }
     
     // Processing by sample
-    void processSample (float x, const int c) {}
+    float processSample (float x, const int c) ;
     
     // Processing by buffer
-    void process (float * buffer, const int N, const int c) {}
+    float process (float * buffer, const int N, const int c) ;
     
+    float FBGain = 0.f; // Affects length of reverb, one value will be applied to all intances of FBCF
+    float APGain = 0.f; // Affects Diffusion, one value will be applied to all intances of APF
     
 private:
     
     double Fs = 48000.f;
-    
-    float FBGain = 0.f; // Affects length of reverb, one value will be applied to all intances of FBCF
-    float APGain = 0.f; // Affects Diffusion, one value will be applied to all intances of APF
     
     // Intances for each FBCF and APF filters labeled accordingly
     FBCF firstDelayBlock;
